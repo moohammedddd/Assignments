2e3912430899797f9dab097cc9f635fd129999fa
moohammedddd <midorafat115566@gmail.com>
2025-01-26 00:13:59 +0200

    Add project files.

---
+++ Assignments/Answer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments
+{
+    internal class Answer
+    {
+        public int AnswerID { get; set; }
+        public string AnswerText { get; set; }
+    }
+}
